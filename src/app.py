63211c08ee43c48b4830189f5e57376ead3a34d9
ShikhaBU <shikhuup99@gmail.com>
2021-01-02 20:39:51 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,118 +1,48 @@
-from fastai import *
-from fastai.vision import *
-import fastai
-import yaml
-import sys
-from io import BytesIO
-from typing import List, Dict, Union, ByteString, Any
-import os
-import flask
-from flask import Flask
-import requests
 import torch
-import json
+from flask_ngrok import run_with_ngrok
+from flask import Flask
+from PIL import Image
+from flask import Flask, render_template, request, redirect, url_for,flash
+import string
+import os
+from CRNN.utils.utils import strLabelConverter, decode_prediction
+from inference import dataextraction, ImageProcessing
 
-with open("src/config.yaml", 'r') as stream:
-    APP_CONFIG = yaml.full_load(stream)
+alphabet = string.digits
+label_converter = strLabelConverter(alphabet)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+UPLOAD_FOLDER = '/content/static/uploads/'
+
 
 app = Flask(__name__)
+run_with_ngrok(app)   #starts ngrok when the app is run
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "shikhaaa"
 
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+  if request.method == 'GET':
+    return render_template('home.html', value='hi')
+  if request.method == 'POST':
+    print(request.files)
+    file = request.files['file']
+    filename = file.filename
+    img = file.read()
+    
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    flash('Image successfully uploaded and displayed')
 
-def load_model(path=".", model_name="model.pkl"):
-    learn = load_learner(path, fname=model_name)
-    return learn
+    img = ImageProcessing(img)
+    pred_text = dataextraction(img, device, label_converter)
+    print(pred_text)
+    return render_template('result.html', reading=pred_text, filename=filename)
 
-
-def load_image_url(url: str) -> Image:
-    response = requests.get(url)
-    img = open_image(BytesIO(response.content))
-    return img
-
-
-def load_image_bytes(raw_bytes: ByteString) -> Image:
-    img = open_image(BytesIO(raw_bytes))
-    return img
-
-
-def predict(img, n: int = 3) -> Dict[str, Union[str, List]]:
-    pred_class, pred_idx, outputs = model.predict(img)
-    pred_probs = outputs / sum(outputs)
-    pred_probs = pred_probs.tolist()
-    predictions = []
-    for image_class, output, prob in zip(model.data.classes, outputs.tolist(), pred_probs):
-        output = round(output, 1)
-        prob = round(prob, 2)
-        predictions.append(
-            {"class": image_class.replace("_", " "), "output": output, "prob": prob}
-        )
-
-    predictions = sorted(predictions, key=lambda x: x["output"], reverse=True)
-    predictions = predictions[0:n]
-    return {"class": str(pred_class), "predictions": predictions}
-
-
-@app.route('/api/classify', methods=['POST', 'GET'])
-def upload_file():
-    if flask.request.method == 'GET':
-        url = flask.request.args.get("url")
-        img = load_image_url(url)
-    else:
-        bytes = flask.request.files['file'].read()
-        img = load_image_bytes(bytes)
-    res = predict(img)
-    return flask.jsonify(res)
-
-
-@app.route('/api/classes', methods=['GET'])
-def classes():
-    classes = sorted(model.data.classes)
-    return flask.jsonify(classes)
-
-
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "pong"
-
-
-@app.route('/config')
-def config():
-    return flask.jsonify(APP_CONFIG)
-
-
-@app.after_request
-def add_header(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-
-    response.cache_control.max_age = 0
-    return response
-
-
-@app.route('/<path:path>')
-def static_file(path):
-    if ".js" in path or ".css" in path:
-        return app.send_static_file(path)
-    else:
-        return app.send_static_file('index.html')
-
-
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
-
-
-def before_request():
-    app.jinja_env.cache = {}
-
-
-model = load_model('models')
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-
-    if "prepare" not in sys.argv:
-        app.jinja_env.auto_reload = True
-        app.config['TEMPLATES_AUTO_RELOAD'] = True
-        app.run(debug=False, host='0.0.0.0', port=port)
-        # app.run(host='0.0.0.0', port=port)
+  app.run()
